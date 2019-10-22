e79049f5d42d334ca1ec98f54b254826fd03fc6a
Link Clinton <LinkClinton@outlook.com>
2019-10-22 12:15:01 +0800

    Add ImGuiWindows to ImGui Extension.

---
+++ Extensions/ImGui/ImGuiWindows.cpp
@@ -0,0 +1,90 @@
+#include "ImGuiWindows.hpp"
+
+#include <Shared/DebugReport.hpp>
+
+CodeRed::ImGuiView::ImGuiView(const ImGuiGenerator& generator) :
+	mImGuiGenerator(generator)
+{
+	
+}
+
+CodeRed::ImGuiWindows::ImGuiWindows(
+	const std::shared_ptr<GpuLogicalDevice>& device,
+	const std::shared_ptr<GpuRenderPass>& renderPass, 
+	const size_t numFrameResources) :
+	mImGuiContext(std::make_shared<ImGuiContext>(device, renderPass, numFrameResources))
+{
+}
+
+void CodeRed::ImGuiWindows::reset(
+	const std::shared_ptr<GpuLogicalDevice>& device,
+	const std::shared_ptr<GpuRenderPass>& renderPass, 
+	const size_t numFrameResources)
+{
+	mImGuiContext.reset();
+	mImGuiContext = std::make_shared<ImGuiContext>(device, renderPass, numFrameResources);
+}
+
+void CodeRed::ImGuiWindows::add(
+	const std::string& windowName, 
+	const std::string& viewName,
+	const std::shared_ptr<ImGuiView>& view)
+{
+	if (mWindows.find(windowName) == mWindows.end())
+		mWindows.insert({ windowName, Views() });
+
+	CODE_RED_DEBUG_WARNING_IF(
+		mWindows[windowName].find(viewName) != mWindows.end(),
+		"the view name is existed in the window, we will replace it."
+	);
+	
+	mWindows[windowName][viewName] = view;
+}
+
+void CodeRed::ImGuiWindows::remove(
+	const std::string& windowName, 
+	const std::string& viewName)
+{
+	if (mWindows.find(windowName) == mWindows.end() || 
+		mWindows[windowName].find(viewName) == mWindows[windowName].end()) {
+		CODE_RED_DEBUG_WARNING("the window or view is not existed, we do not remove anything.")
+		
+		return;
+	}
+
+	mWindows[windowName].erase(viewName);
+
+	if (mWindows[windowName].empty()) mWindows.erase(windowName);
+}
+
+void CodeRed::ImGuiWindows::draw(const std::shared_ptr<GpuGraphicsCommandList>& commandList)
+{
+	std::vector<std::pair<std::string, std::string>> removeList;
+	
+	for (auto& window : mWindows) {
+		//drawing the window
+		ImGui::Begin(window.first.c_str());
+		
+		for (auto& weakView : window.second) {
+			ImGui::TreeNode(weakView.first.c_str());
+			
+			//if the imgui view is deleted, we will remove the view
+			if (weakView.second.expired()) removeList.push_back({ window.first, weakView.first });
+			else {
+				//drawing
+				weakView.second.lock()->generator()();
+			}
+
+			ImGui::TreePop();
+		}
+
+		ImGui::End();
+	}
+
+	//remove views that is deleted.
+	for (auto view : removeList) remove(view.first, view.second);
+
+	ImGui::Render();
+
+	mImGuiContext->draw(commandList, ImGui::GetDrawData());
+}
---
+++ Extensions/ImGui/ImGuiWindows.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "ImGuiContext.hpp"
+
+#include <unordered_map>
+#include <functional>
+
+namespace CodeRed {
+
+	using ImGuiGenerator = std::function<void()>;
+	
+	class ImGuiView final : public Noncopyable {
+	public:
+		explicit ImGuiView(
+			const ImGuiGenerator& generator);
+
+		auto generator() const -> const ImGuiGenerator& { return mImGuiGenerator; }
+	private:
+		ImGuiGenerator mImGuiGenerator;
+	};
+
+	class ImGuiWindows final : Noncopyable {
+	public:
+		explicit ImGuiWindows(
+			const std::shared_ptr<GpuLogicalDevice>& device,
+			const std::shared_ptr<GpuRenderPass>& renderPass,
+			const size_t numFrameResources = 2);
+
+		~ImGuiWindows() = default;
+
+		void reset(
+			const std::shared_ptr<GpuLogicalDevice>& device,
+			const std::shared_ptr<GpuRenderPass>& renderPass,
+			const size_t numFrameResources = 2);
+
+		void add(
+			const std::string& windowName,
+			const std::string& viewName,
+			const std::shared_ptr<ImGuiView>& view);
+
+		void remove(
+			const std::string& windowName,
+			const std::string& viewName);
+
+		void draw(
+			const std::shared_ptr<GpuGraphicsCommandList>& commandList);
+	private:
+		using Views = std::unordered_map<std::string, std::weak_ptr<ImGuiView>>;
+		
+		std::shared_ptr<ImGuiContext> mImGuiContext;
+
+		std::unordered_map<std::string, Views> mWindows;
+	};
+	
+}
